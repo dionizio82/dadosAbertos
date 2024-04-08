65896f6b85725637959eeabc0c6147cb2b7fc8fd
dionizio82 <contato@heberdionizio.com.br>
2024-04-08 19:50:17 -0300

    Correção da importação de declarative_base e Base em todos os arquivos. Zero problemas no momento. #paz

--- empresas.py
+++ empresas.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, Date
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
--- estabelecimentos.py
+++ estabelecimentos.py
@@ -1,6 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy import ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 class Estabelecimento(Base):
     __tablename__ = 'estabelecimentos'
