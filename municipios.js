79f38ccbcd10c047f84afb45524915000e22ff88
Heber Dionizio de Oliveira <contato@heberdionizio.com.br>
2024-05-12 23:37:42 -0300

    Arquivos de importação de municipios e socios.

---
+++ municipios.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'dados_abertos',
+  password: '852518',
+  port: 5432,
+  client_encoding: 'utf8'
+});
+
+async function importCSV() {
+  const client = await pool.connect();
+  try {
+    const tableName = 'municipio';
+    const csvFilePath = 'municipios.csv';
+
+    const fileStream = fs.createReadStream(csvFilePath, { encoding: 'utf8' })
+      .pipe(csv({
+        separator: ';',
+        quote: '"',
+        escape: '"',
+        headers: ['codigo', 'descricao']
+      }));
+
+    for await (const row of fileStream) {
+      await client.query(
+        `INSERT INTO ${tableName} (codigo, descricao)
+         VALUES ($1, $2)
+         ON CONFLICT (codigo) DO UPDATE SET
+         descricao = EXCLUDED.descricao, updated_at = CURRENT_TIMESTAMP`,
+        [row.codigo, row.descricao]
+      );
+    }
+    console.log('Importação de dados concluída.');
+  } catch (error) {
+    console.error('Erro ao importar CSV:', error.message);
+  } finally {
+    client.release();
+  }
+}
+
+importCSV();
---
+++ socio.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'dados_abertos',
+  password: '852518',
+  port: 5432,
+  client_encoding: 'utf8'
+});
+
+const logStream = fs.createWriteStream('socioImportacao.log', { flags: 'a' });
+
+const logError = (cnpj, cpfCnpj, error) => {
+  const message = `${new Date().toISOString()} - Erro no CNPJ ${cnpj}, CPF/CNPJ do Sócio ${cpfCnpj}: ${error}\n`;
+  logStream.write(message);
+  console.error(message);
+};
+
+const processRecord = async (data) => {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+
+    const { cnpj_basico, identificador_socio, nome_socio, cpf_cnpj_socio, qualificacao_socio, data_entrada, pais_socio, rep_legal, nome_rep_legal, qualificacao_rep_legal, faixa_etaria } = data;
+    const queryParams = [
+      cnpj_basico, cpf_cnpj_socio, identificador_socio, nome_socio, qualificacao_socio,
+      data_entrada ? new Date(data_entrada.substring(0, 4), data_entrada.substring(4, 6) - 1, data_entrada.substring(6, 8)) : null,
+      pais_socio, rep_legal, nome_rep_legal, qualificacao_rep_legal, faixa_etaria
+    ];
+
+    await client.query(`
+      INSERT INTO socio (cnpj_basico, cpf_cnpj_socio, identificador_socio, nome_socio, qualificacao_socio, data_entrada, pais_socio, rep_legal, nome_rep_legal, qualificacao_rep_legal, faixa_etaria, created_at, updated_at)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+      ON CONFLICT (cnpj_basico, cpf_cnpj_socio) DO UPDATE SET
+      identificador_socio = EXCLUDED.identificador_socio, nome_socio = EXCLUDED.nome_socio, qualificacao_socio = EXCLUDED.qualificacao_socio, data_entrada = EXCLUDED.data_entrada, pais_socio = EXCLUDED.pais_socio, rep_legal = EXCLUDED.rep_legal, nome_rep_legal = EXCLUDED.nome_rep_legal, qualificacao_rep_legal = EXCLUDED.qualificacao_rep_legal, faixa_etaria = EXCLUDED.faixa_etaria, updated_at = CURRENT_TIMESTAMP`, queryParams);
+
+    await client.query('COMMIT');
+  } catch (error) {
+    await client.query('ROLLBACK');
+    logError(cnpj_basico, cpf_cnpj_socio, error.message);
+  } finally {
+    client.release();
+  }
+};
+
+const processCSV = async () => {
+  const fileStream = fs.createReadStream('socios.csv', { encoding: 'utf8' })
+    .pipe(csv({
+      separator: ';',
+      headers: ['cnpj_basico', 'identificador_socio', 'nome_socio', 'cpf_cnpj_socio', 'qualificacao_socio', 'data_entrada', 'pais_socio', 'rep_legal', 'nome_rep_legal', 'qualificacao_rep_legal', 'faixa_etaria']
+    }));
+
+  for await (const row of fileStream) {
+    console.log(`Processando CNPJ: ${row.cnpj_basico}, CPF/CNPJ do Sócio: ${row.cpf_cnpj_socio}`);
+    await processRecord(row);
+  }
+  console.log('Importação de dados concluída.');
+};
+
+processCSV();
